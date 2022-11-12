0f8c7b4e6d3e25df3cc89af164ec621cc24e3d94
Moises Ramirez <moises.ramon.ramirez@gmail.com>
2022-11-12 13:43:30 -0400

    bugs

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -7,7 +7,7 @@ export default function operate(numberOne, numberTwo, operation) {
     return one.plus(two).toString();
   }
   if (operation === "-") {
-    return one.minus(two).toString();
+    return one.plus(two).toString();
   }
   if (operation === "x") {
     return one.times(two).toString();
